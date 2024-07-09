bef57cfce4888c83edc1ab13d7b7ecd34064a372
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-09 16:34:00 +0200

    Initial commit

---
+++ storeez/src/main/java/pl/storeez/StoreezApplication.java
@@ -0,0 +1,13 @@
+package pl.storeez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoreezApplication.class, args);
+    }
+
+}
---
+++ storeez/src/test/java/pl/storeez/StoreezApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.storeez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
