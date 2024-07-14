8a0bedd807efcd1eb1389b8e99ea5590c33334fc
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-14 20:00:21 +0200

    Change test datas for category and clothes

--- storeez/src/main/resources/db/testdata/0001_category.sql
+++ storeez/src/main/resources/db/testdata/0001_category.sql
@@ -1,5 +1,11 @@
-insert into category (name) values ('T-shirt');
-insert into category (name) values ('Sweatshirt');
-insert into category (name) values ('Hoodie');
-insert into category (name) values ('Trousers');
-insert into category (name) values ('Boots');
+-- main category
+insert into category (name) values ('Clothes');
+insert into category (name) values ('Books');
+insert into category (name) values ('Perfumes');
+
+-- subcategory
+insert into subcategory (name, category_id) values ('T-shirt', 1);
+insert into subcategory (name, category_id) values ('Sweatshirt', 1);
+insert into subcategory (name, category_id) values ('Hoodie', 1);
+insert into subcategory (name, category_id) values ('Trousers', 1);
+insert into subcategory (name, category_id) values ('Boots', 1);
