9deacbb188ed8fa6f674da21ad0abaa8042301e7
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-14 18:15:26 +0200

    Add test datas for category and clothes

---
+++ storeez/src/main/resources/db/testdata/0001_category.sql
@@ -0,0 +1,5 @@
+insert into category (name) values ('T-shirt');
+insert into category (name) values ('Sweatshirt');
+insert into category (name) values ('Hoodie');
+insert into category (name) values ('Trousers');
+insert into category (name) values ('Boots');
