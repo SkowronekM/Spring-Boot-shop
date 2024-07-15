a9f011fecbae0281bc2cf9cbdca85b86dbbee647
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-15 18:40:07 +0200

    Create CategoryDto class and CategoryDtoMapper

---
+++ storeez/src/main/java/pl/storeez/domain/categories/CategoryDtoMapper.java
@@ -0,0 +1,22 @@
+package pl.storeez.domain.categories;
+
+import pl.storeez.domain.categories.dto.CategoryDto;
+import pl.storeez.domain.categories.dto.SubcategoryDto;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CategoryDtoMapper {
+    static CategoryDto map(Category category) {
+        List<SubcategoryDto> subcategoryDtos = category.getSubcategories()
+                .stream()
+                .map(subcategory -> new SubcategoryDto(subcategory.getId(), subcategory.getName()))
+                .collect(Collectors.toList());
+
+        return new CategoryDto(
+                category.getId(),
+                category.getName(),
+                subcategoryDtos
+        );
+    }
+}
---
+++ storeez/src/main/java/pl/storeez/domain/categories/dto/CategoryDto.java
@@ -0,0 +1,39 @@
+package pl.storeez.domain.categories.dto;
+
+import java.util.List;
+
+public class CategoryDto {
+    private Long id;
+    private String name;
+    private List<SubcategoryDto> subcategories;
+
+    public CategoryDto(Long id, String name, List<SubcategoryDto> subcategories) {
+        this.id = id;
+        this.name = name;
+        this.subcategories = subcategories;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<SubcategoryDto> getSubcategories() {
+        return subcategories;
+    }
+
+    public void setSubcategories(List<SubcategoryDto> subcategories) {
+        this.subcategories = subcategories;
+    }
+}
