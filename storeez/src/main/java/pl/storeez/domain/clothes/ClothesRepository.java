68ad1ce76692a2dce0d8e5cbd8f2a923ded39a29
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-15 18:38:37 +0200

    Add new method to find all clothes by category

--- storeez/src/main/java/pl/storeez/domain/clothes/ClothesRepository.java
+++ storeez/src/main/java/pl/storeez/domain/clothes/ClothesRepository.java
@@ -1,9 +1,15 @@
 package pl.storeez.domain.clothes;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ClothesRepository extends CrudRepository<Clothes, Long> {
     List<Clothes> findAllByPromotedIsTrue();
+
+    @Query("SELECT c FROM Clothes c WHERE LOWER(c.subcategory.category.name) = LOWER(:categoryName)")
+    List<Clothes> findAllByCategoryNameIgnoreCase(@Param("categoryName") String categoryName);
 }
