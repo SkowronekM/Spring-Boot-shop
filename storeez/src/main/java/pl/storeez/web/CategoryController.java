4d1d7dc1b2b57b7ef6d0b089baffd2e80aed32ae
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-16 21:05:31 +0200

    Some changes

--- storeez/src/main/java/pl/storeez/web/CategoryController.java
+++ storeez/src/main/java/pl/storeez/web/CategoryController.java
@@ -1,35 +1,32 @@
 package pl.storeez.web;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
+import pl.storeez.domain.categories.CategoryDtoMapper;
 import pl.storeez.domain.categories.CategoryService;
-import pl.storeez.domain.categories.SubcategoryService;
 import pl.storeez.domain.categories.dto.CategoryDto;
-import pl.storeez.domain.categories.dto.SubcategoryDto;
+import pl.storeez.domain.clothes.ClothesDtoMapper;
 import pl.storeez.domain.clothes.ClothesService;
 import pl.storeez.domain.clothes.dto.ClothesDto;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class CategoryController {
     private final ClothesService clothesService;
     private final CategoryService categoryService;
-    private final SubcategoryService subcategoryService;
 
-    public CategoryController(ClothesService clothesService, CategoryService categoryService, SubcategoryService subcategoryService) {
+    public CategoryController(ClothesService clothesService, CategoryService categoryService) {
         this.clothesService = clothesService;
         this.categoryService = categoryService;
-        this.subcategoryService = subcategoryService;
     }
 
-    @GetMapping("/category/{name}")
+    @GetMapping("/{name}")
     public String getCategory(@PathVariable String name, Model model) {
         CategoryDto category = categoryService.findCategoryByName(name)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
@@ -40,7 +37,7 @@ public class CategoryController {
         model.addAttribute("clothes", clothes);
         return "main-site";
     }
-    
+
     @GetMapping("/category/{name}/{subcategory}s")
     public String getSubcategory(@PathVariable String name, @PathVariable String subcategory, Model model) {
         CategoryDto category = categoryService.findCategoryByName(name)
