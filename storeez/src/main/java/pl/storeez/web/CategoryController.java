4196fbad100fc0a0b77d2307b4813a1e59c9c664
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-16 21:09:34 +0200

    Some changes in category controller

--- storeez/src/main/java/pl/storeez/web/CategoryController.java
+++ storeez/src/main/java/pl/storeez/web/CategoryController.java
@@ -6,27 +6,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
-import pl.storeez.domain.categories.CategoryDtoMapper;
 import pl.storeez.domain.categories.CategoryService;
+import pl.storeez.domain.categories.SubcategoryService;
 import pl.storeez.domain.categories.dto.CategoryDto;
-import pl.storeez.domain.clothes.ClothesDtoMapper;
+import pl.storeez.domain.categories.dto.SubcategoryDto;
 import pl.storeez.domain.clothes.ClothesService;
 import pl.storeez.domain.clothes.dto.ClothesDto;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 public class CategoryController {
     private final ClothesService clothesService;
     private final CategoryService categoryService;
+    private final SubcategoryService subcategoryService;
 
-    public CategoryController(ClothesService clothesService, CategoryService categoryService) {
+    public CategoryController(ClothesService clothesService, CategoryService categoryService, SubcategoryService subcategoryService) {
         this.clothesService = clothesService;
         this.categoryService = categoryService;
+        this.subcategoryService = subcategoryService;
     }
 
-    @GetMapping("/{name}")
+    @GetMapping("/category/{name}")
     public String getCategory(@PathVariable String name, Model model) {
         CategoryDto category = categoryService.findCategoryByName(name)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
