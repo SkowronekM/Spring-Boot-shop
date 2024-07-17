b4390ad39fae63daa97bac124a367871943e6942
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-17 17:49:50 +0200

    Add method to add subcategory

--- storeez/src/main/java/pl/storeez/domain/categories/SubcategoryService.java
+++ storeez/src/main/java/pl/storeez/domain/categories/SubcategoryService.java
@@ -1,6 +1,7 @@
 package pl.storeez.domain.categories;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.storeez.domain.categories.dto.SubcategoryDto;
 
 import java.util.Optional;
@@ -8,12 +9,29 @@ import java.util.Optional;
 @Service
 public class SubcategoryService {
     private final SubcategoryRepository subcategoryRepository;
+    private final CategoryRepository categoryRepository;
 
-    public SubcategoryService(SubcategoryRepository subcategoryRepository) {
+    public SubcategoryService(SubcategoryRepository subcategoryRepository, CategoryRepository categoryRepository) {
         this.subcategoryRepository = subcategoryRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     public Optional<SubcategoryDto> findSubcategoryByName(String name) {
         return subcategoryRepository.findByNameIgnoreCase(name).map(SubcategoryDtoMapper::map);
     }
+
+    @Transactional
+    public SubcategoryDto addSubcategory(SubcategoryDto subcategoryDto) {
+        Category category = categoryRepository.findById(subcategoryDto.getCategoryId())
+                .orElseThrow(() -> new RuntimeException("Category not found!"));
+
+        Subcategory subcategoryToSave = new Subcategory();
+        subcategoryToSave.setName(subcategoryDto.getName());
+        subcategoryToSave.setCategory(category);
+        subcategoryToSave = subcategoryRepository.save(subcategoryToSave);
+
+        subcategoryDto.setId(subcategoryToSave.getId());
+        return subcategoryDto;
+    }
+
 }
