076d040e20a9dd302f4a8713dc7c031272e67731
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-17 12:32:21 +0200

    Create method to add new category

--- storeez/src/main/java/pl/storeez/domain/categories/CategoryService.java
+++ storeez/src/main/java/pl/storeez/domain/categories/CategoryService.java
@@ -1,7 +1,9 @@
 package pl.storeez.domain.categories;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import pl.storeez.domain.categories.dto.CategoryDto;
+import pl.storeez.domain.categories.dto.SubcategoryDto;
 
 import java.util.Optional;
 
@@ -15,5 +17,13 @@ public class CategoryService {
 
     public Optional<CategoryDto> findCategoryByName(String name) {
         return categoryRepository.findByNameIgnoreCase(name).map(CategoryDtoMapper::map);
+    }
+
+    @Transactional
+    public void addCategory(CategoryDto categoryDto) {
+        Category categoryToSave = new Category();
+        categoryToSave.setId(categoryDto.getId());
+        categoryToSave.setName(categoryDto.getName());
+        categoryRepository.save(categoryToSave);
     }
 }
