b378d48fe886bfd2ba8f924ae9c1c62b41e30a29
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-17 12:31:12 +0200

    Change path to admin controller

--- storeez/src/main/java/pl/storeez/web/admin/AdminController.java
+++ storeez/src/main/java/pl/storeez/web/admin/AdminController.java
@@ -1,10 +1,12 @@
-package pl.storeez.web;
+package pl.storeez.web.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+
+    public final static String NOTIFICATION_ATTR = "notification";
 
     @GetMapping("/admin")
     public String getAdminPanel() {
