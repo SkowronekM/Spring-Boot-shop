04e2e49dfb7895313c05e6cf51c4523804426808
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-23 21:22:12 +0200

    Add method to find all categories order by id

--- storeez/src/main/java/pl/storeez/domain/categories/CategoryRepository.java
+++ storeez/src/main/java/pl/storeez/domain/categories/CategoryRepository.java
@@ -2,8 +2,10 @@ package pl.storeez.domain.categories;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
     Optional<Category> findByNameIgnoreCase(String name);
+    List<Category> findAllByOrderByIdAsc();
 }
