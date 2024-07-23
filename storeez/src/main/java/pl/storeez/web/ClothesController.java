ece1bf67cb12702e74608666b4d13a023a7ca6f4
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-23 17:33:40 +0200

    Change type of discounted price in clothes controller

--- storeez/src/main/java/pl/storeez/web/ClothesController.java
+++ storeez/src/main/java/pl/storeez/web/ClothesController.java
@@ -9,7 +9,7 @@ import org.springframework.web.server.ResponseStatusException;
 import pl.storeez.domain.clothes.ClothesService;
 import pl.storeez.domain.clothes.dto.ClothesDto;
 
-import java.util.Optional;
+import java.math.BigDecimal;
 
 @Controller
 public class ClothesController {
@@ -23,6 +23,10 @@ public class ClothesController {
     public String getClothes(@PathVariable String subcategory, @PathVariable long id, Model model) {
         ClothesDto clothes = clothesService.findClothesById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
+        BigDecimal discountedPrice = clothesService.getDiscountedPrice(clothes);
+        clothes.setPriceAfterDiscount(discountedPrice);
+
         model.addAttribute("clothes", clothes);
         model.addAttribute("heading", "Product details");
 
