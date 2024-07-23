5d3caebea83392f9628e6bfc8dc9d78757f6520e
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-23 17:36:14 +0200

    Some changes in add new clothes controller

--- storeez/src/main/java/pl/storeez/web/admin/AddNewClothesManagementController.java
+++ storeez/src/main/java/pl/storeez/web/admin/AddNewClothesManagementController.java
@@ -5,14 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.storeez.domain.categories.CategoryService;
-import pl.storeez.domain.categories.Subcategory;
-import pl.storeez.domain.categories.SubcategoryRepository;
 import pl.storeez.domain.categories.SubcategoryService;
-import pl.storeez.domain.categories.dto.CategoryDto;
 import pl.storeez.domain.clothes.ClothesService;
 import pl.storeez.domain.clothes.dto.ClothesDto;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
@@ -20,13 +15,11 @@ public class AddNewClothesManagementController {
     private final ClothesService clothesService;
     private final CategoryService categoryService;
     private final SubcategoryService subcategoryService;
-    private final SubcategoryRepository subcategoryRepository;
 
-    public AddNewClothesManagementController(ClothesService clothesService, CategoryService categoryService, SubcategoryService subcategoryService, SubcategoryRepository subcategoryRepository) {
+    public AddNewClothesManagementController(ClothesService clothesService, CategoryService categoryService, SubcategoryService subcategoryService) {
         this.clothesService = clothesService;
         this.categoryService = categoryService;
         this.subcategoryService = subcategoryService;
-        this.subcategoryRepository = subcategoryRepository;
     }
 
     @GetMapping("/add-clothes")
@@ -37,13 +30,6 @@ public class AddNewClothesManagementController {
         model.addAttribute("subcategories", subcategoryService.getAllSubcategories());
         return "admin/add-clothes-form";
     }
-
-//    @GetMapping("/subcategories")
-//    @ResponseBody
-//    public List<Subcategory> getSubcategories(@RequestParam Long categoryId) {
-//        List<Subcategory> subcategories = subcategoryRepository.findByCategoryId(categoryId);
-//        return subcategories;
-//    }
 
     @PostMapping("/add-clothes")
     public String addClothes(ClothesDto clothesDto, RedirectAttributes redirectAttributes) {
