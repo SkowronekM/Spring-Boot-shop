d67764d38d34d66af30045b0c30588b1e166a8b9
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-23 21:22:35 +0200

    Add methods to find subcategories

--- storeez/src/main/java/pl/storeez/domain/categories/SubcategoryRepository.java
+++ storeez/src/main/java/pl/storeez/domain/categories/SubcategoryRepository.java
@@ -2,9 +2,12 @@ package pl.storeez.domain.categories;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface SubcategoryRepository extends CrudRepository<Subcategory, Long> {
     Optional<Subcategory> findByName(String name);
     Optional<Subcategory> findByNameIgnoreCase(String name);
+    List<Subcategory> findAllByOrderByIdAsc();
+    List<Subcategory> findByCategoryId(Long categoryId);
 }
