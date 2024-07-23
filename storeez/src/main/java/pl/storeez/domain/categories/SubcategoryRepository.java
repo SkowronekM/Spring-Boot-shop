d23718f5f6326a28d1d9951284b0fe0b95ccb21b
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-23 17:28:46 +0200

    Add new method to subcategory repository

--- storeez/src/main/java/pl/storeez/domain/categories/SubcategoryRepository.java
+++ storeez/src/main/java/pl/storeez/domain/categories/SubcategoryRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.Optional;
 
 public interface SubcategoryRepository extends CrudRepository<Subcategory, Long> {
+    Optional<Subcategory> findByName(String name);
     Optional<Subcategory> findByNameIgnoreCase(String name);
 }
