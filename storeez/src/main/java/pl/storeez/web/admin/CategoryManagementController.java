84d6ff4d3443cc2415604e37e1211a1407f4a61d
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-23 21:27:40 +0200

    Add new methods to category and subcategory management controllers to edit / manage them

--- storeez/src/main/java/pl/storeez/web/admin/CategoryManagementController.java
+++ storeez/src/main/java/pl/storeez/web/admin/CategoryManagementController.java
@@ -2,20 +2,27 @@ package pl.storeez.web.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import pl.storeez.domain.categories.CategoryService;
+import pl.storeez.domain.categories.*;
 import pl.storeez.domain.categories.dto.CategoryDto;
+import pl.storeez.domain.clothes.ClothesService;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class CategoryManagementController {
     private final CategoryService categoryService;
+    private final CategoryRepository categoryRepository;
+    private final SubcategoryRepository subcategoryRepository;
+    private final ClothesService clothesService;
 
-    public CategoryManagementController(CategoryService categoryService) {
+    public CategoryManagementController(CategoryService categoryService, CategoryRepository categoryRepository, SubcategoryRepository subcategoryRepository, ClothesService clothesService) {
         this.categoryService = categoryService;
+        this.categoryRepository = categoryRepository;
+        this.subcategoryRepository = subcategoryRepository;
+        this.clothesService = clothesService;
     }
 
     @GetMapping("/add-category")
@@ -30,6 +37,47 @@ public class CategoryManagementController {
     public String addCategory(CategoryDto category, RedirectAttributes redirectAttributes) {
         categoryService.addCategory(category);
         redirectAttributes.addFlashAttribute(AdminController.NOTIFICATION_ATTR, "Category %s has been added".formatted(category.getName()));
+
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/manage-categories")
+    public String manageCategoryTable(Model model) {
+        Iterable<Category> categories = categoryRepository.findAllByOrderByIdAsc();
+        model.addAttribute("categories", categories);
+
+        return "admin/manage-categories";
+    }
+
+    @GetMapping("/edit-category/{id}")
+    public String editCategoryTable(@PathVariable Long id, Model model) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid category Id:" + id));
+        model.addAttribute("category", category);
+
+        return "admin/edit-category";
+    }
+
+    @PostMapping("/edit-category")
+    public String editCategory(@ModelAttribute Category category, RedirectAttributes redirectAttributes) {
+        categoryRepository.save(category);
+        redirectAttributes.addFlashAttribute(AdminController.NOTIFICATION_ATTR, "Category name %s has been changed".formatted(category.getName()));
+
+        return "redirect:/admin";
+    }
+
+    @GetMapping("/delete-category/{id}")
+    public String deleteCategory(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+        List<Subcategory> subcategories = subcategoryRepository.findByCategoryId(id);
+        for (Subcategory subcategory : subcategories) {
+            clothesService.deleteClothesBySubcategory(subcategory.getId());
+            subcategoryRepository.delete(subcategory);
+        }
+
+        categoryRepository.delete(category);
+        redirectAttributes.addFlashAttribute(AdminController.NOTIFICATION_ATTR, "Category %s and all links have been deleted".formatted(category.getName()));
 
         return "redirect:/admin";
     }
--- storeez/src/main/java/pl/storeez/web/admin/SubcategoryManagementController.java
+++ storeez/src/main/java/pl/storeez/web/admin/SubcategoryManagementController.java
@@ -3,23 +3,30 @@ package pl.storeez.web.admin;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import pl.storeez.domain.categories.Category;
-import pl.storeez.domain.categories.CategoryService;
-import pl.storeez.domain.categories.SubcategoryService;
+import pl.storeez.domain.categories.*;
 import pl.storeez.domain.categories.dto.SubcategoryDto;
+import pl.storeez.domain.clothes.ClothesService;
+
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @RequestMapping("/admin")
 public class SubcategoryManagementController {
-    private CategoryService categoryService;
-    private SubcategoryService subcategoryService;
+    private final CategoryService categoryService;
+    private final SubcategoryService subcategoryService;
+    private final SubcategoryRepository subcategoryRepository;
+    private final ClothesService clothesService;
 
-    public SubcategoryManagementController(CategoryService categoryService, SubcategoryService subcategoryService) {
+    public SubcategoryManagementController(CategoryService categoryService, SubcategoryService subcategoryService, SubcategoryRepository subcategoryRepository, ClothesService clothesService) {
         this.categoryService = categoryService;
         this.subcategoryService = subcategoryService;
+        this.subcategoryRepository = subcategoryRepository;
+        this.clothesService = clothesService;
     }
 
     @GetMapping("/add-subcategory")
@@ -37,8 +44,34 @@ public class SubcategoryManagementController {
                 .formatted(subcategoryDto.getName()));
 
         return "redirect:/admin";
-
     }
 
+    @GetMapping("/manage-subcategories")
+    public String manageSubcategoryTable(Model model) {
+        List<Subcategory> subcategories = subcategoryRepository.findAllByOrderByIdAsc();
+        model.addAttribute("subcategories", subcategories);
+
+        return "admin/manage-subcategories";
+    }
+
+    @GetMapping("/edit-subcategory/{id}")
+    public String editSubcategoryTable(@PathVariable Long id, Model model) {
+        Iterable<Subcategory> subcategories = subcategoryRepository.findAllById(Collections.singleton(id));
+        model.addAttribute("subcategories", subcategories);
+
+        return "admin/edit-subcategory";
+    }
+
+    @GetMapping("/delete-subcategory/{id}")
+    public String deleteSubcategory(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        Subcategory subcategory = subcategoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found"));
+
+        clothesService.deleteClothesBySubcategory(id);
+        subcategoryRepository.delete(subcategory);
+        redirectAttributes.addFlashAttribute(AdminController.NOTIFICATION_ATTR, "Subcategory %s and all links have been deleted".formatted(subcategory.getName()));
+
+        return "redirect:/admin";
+    }
 
 }
