0f2a385dcc06bafcbfaab4538ffd7e6a4d67e76d
Michał Skowronek <michalskowronek0200@gmail.com>
2024-07-25 19:50:47 +0200

    Create new dto ClothesEditDto to edit clothes

---
+++ storeez/src/main/java/pl/storeez/domain/clothes/dto/ClothesEditDto.java
@@ -0,0 +1,164 @@
+package pl.storeez.domain.clothes.dto;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
+public class ClothesEditDto {
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private BigDecimal price;
+    private int discount;
+    private BigDecimal priceAfterDiscount;
+    private int stock;
+    private String category;
+    private Long subcategory;
+    private boolean promoted;
+    private String size;
+    private String color;
+    private String material;
+    private MultipartFile image;
+
+    public ClothesEditDto() {
+    }
+
+    public ClothesEditDto(Long id, String name, String brand, String description, BigDecimal price, int discount, BigDecimal priceAfterDiscount, int stock, String category, Long subcategory, boolean promoted, String size, String color, String material, MultipartFile image) {
+        this.id = id;
+        this.name = name;
+        this.brand = brand;
+        this.description = description;
+        this.price = price;
+        this.discount = discount;
+        this.priceAfterDiscount = priceAfterDiscount;
+        this.stock = stock;
+        this.category = category;
+        this.subcategory = subcategory;
+        this.promoted = promoted;
+        this.size = size;
+        this.color = color;
+        this.material = material;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    public BigDecimal getPriceAfterDiscount() {
+        return priceAfterDiscount;
+    }
+
+    public void setPriceAfterDiscount(BigDecimal priceAfterDiscount) {
+        this.priceAfterDiscount = priceAfterDiscount;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Long getSubcategory() {
+        return subcategory;
+    }
+
+    public void setSubcategory(Long subcategory) {
+        this.subcategory = subcategory;
+    }
+
+    public boolean isPromoted() {
+        return promoted;
+    }
+
+    public void setPromoted(boolean promoted) {
+        this.promoted = promoted;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public MultipartFile getImage() {
+        return image;
+    }
+
+    public void setImage(MultipartFile image) {
+        this.image = image;
+    }
+}
